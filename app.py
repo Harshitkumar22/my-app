ddb7c92d71bc1211deae81ff4c263933e5e73034
“[Harshitkumar22]” <[harshitkumar1463@gmail.com]>
2026-04-24 19:27:50 +0530

    Uploading Jenkins & Neccesssary files for Pipleline

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # return "Hello from CI/CD Pipeline!"
+    return "Hello from CI/CD Pipeline!, my sapid is 500122407"
+
+app.run(host="0.0.0.0", port=80)
+
